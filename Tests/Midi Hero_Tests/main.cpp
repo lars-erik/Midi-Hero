e6e4b22183c9932959bb167d13bc3f996dedec2a
Lars-Erik <lars-erik@aabech.no>
2025-01-29 21:34:00 +0100

    Hopeful attempt for dev build and failed test project cmake things

--- Tests/Midi Hero_Tests/main.cpp
+++ Tests/Midi Hero_Tests/main.cpp
@@ -1,8 +1,8 @@
-#include "catch_amalgamated.hpp"
+#include <catch_amalgamated.hpp>
 
 #define APPROVALS_CATCH_EXISTING_MAIN
 #define APPROVALS_CATCH2_V3_AMALGAMATED
-#include "ApprovalTests.v.10.13.0.hpp"
+#include <ApprovalTests.v.10.13.0.hpp>
 
 int main(int argc, char* argv[]) {
     // your setup ...
