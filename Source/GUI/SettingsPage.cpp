f05e993aa087763d84efe849a2bc4945ee445f49
Lars-Erik <lars-erik@aabech.no>
2025-02-08 12:56:48 +0100

    Three value slider layout suggestion for timing settings

--- Source/GUI/SettingsPage.cpp
+++ Source/GUI/SettingsPage.cpp
@@ -1,22 +1,30 @@
 #include "SettingsPage.h"
 
+
 SettingsPage::SettingsPage(shared_ptr<MidiHeroSettings> const&)
 {
-    Rectangle<int> layoutArea{ 20, 20, 580, 430 };
+    auto layoutArea = getBounds();
+    layoutArea.reduce(10, 10);
 
     auto* s = new Slider();
     children.add(s);
     addAndMakeVisible(s);
 
-    s->setRange(0, 160, 1);
-    s->setSliderStyle(Slider::LinearHorizontal);
-    s->setTextBoxStyle(Slider::TextBoxRight, false, 50, 20);
+    s->setSliderStyle(Slider::ThreeValueHorizontal);
+    s->setRange(0, 80, 1);
+    s->setMaxValue(40);
+    s->setValue(20);
+    s->setMinValue(10);
+
+    s->setTextBoxStyle(Slider::TextBoxBelow, false, 50, 20);
     s->setTextValueSuffix("ms");
-    s->setBounds(layoutArea.removeFromBottom(400));
-    s->setDoubleClickReturnValue(true, 80);
+    s->setBounds(layoutArea.removeFromTop(50));
+    s->setDoubleClickReturnValue(true, 40);
 }
 
 void SettingsPage::resized()
 {
-
+    auto layoutArea = getBounds();
+    layoutArea.reduce(10, 10);
+    children[0]->setBounds(layoutArea.removeFromTop(50));
 }
